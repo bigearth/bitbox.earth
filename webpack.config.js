f72b9e1496c33e7b32e6d05926ab374a98b55d76
Gabriel Cardona <gabriel.cardona@clone.earth>
2018-01-31 05:41:16 -0800

    build.

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,32 @@
 let path = require('path');
+let HtmlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = {
-    entry: './src/index.js',
-    output: {
-        path: path.resolve(__dirname, 'build'),
-        filename: 'bundle.js'
-    },
-    module: {
-        loaders: [
-            {
-                test: /\.js$/,
-                loader: 'babel-loader',
-                query: {
-                    presets: ['es2015', 'react']
-                },
-                exclude: /node_modules/
-            },
-            {
-              test: /\.css$/,
-              use: [ 'style-loader', 'css-loader' ]
-            }
-        ]
-    }
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [{
+        test: /\.js$/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015', 'react']
+        },
+        exclude: /node_modules/
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'BITBOX',
+      template: 'src/index.html',
+      filename: 'index.html'
+    })
+  ]
 };
