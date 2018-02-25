327a0d4693dd045135836fc923a676216a4afd4c
Gabriel Cardona <gabriel.cardona@clone.earth>
2018-02-25 11:38:09 -0800

    v.

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -18,7 +18,7 @@ class Homepage extends Component {
               SUPERCHARGE your Bitcoin Cash workflow.
             </p>
             <p>
-              <a href="https://github.com/bigearth/bitbox-electron/releases/download/0.1.4/BITBOX-0.1.4.dmg" className="pure-button pure-button-primary">
+              <a href="https://github.com/bigearth/bitbox-electron/releases/download/0.1.5/BITBOX-0.1.5.dmg" className="pure-button pure-button-primary">
                 <i className="fab fa-apple"></i> Download (MacOS)
               </a>
             </p>
@@ -82,7 +82,7 @@ class Homepage extends Component {
               </div>
               <div className="pure-u-1 pure-u-md-1-2 pure-u-lg-8-24 screenshot-desc">
                 <p>
-                  <a href="https://github.com/bigearth/bitbox-electron/releases/download/0.1.4/BITBOX-0.1.4.dmg" className="pure-button pure-button-primary">
+                  <a href="https://github.com/bigearth/bitbox-electron/releases/download/0.1.5/BITBOX-0.1.5.dmg" className="pure-button pure-button-primary">
                     <i className="fab fa-apple"></i> Download (MacOS)
                   </a>
                 </p>
@@ -121,7 +121,7 @@ class Homepage extends Component {
           </div>
 
           <div className="footer l-box is-center">
-            Created w/ ❤️ on EARTH by <a href='https://twitter.com/cgcardona'>Gabriel Cardona</a>
+            Created w/ ❤️ on EARTH by <a href='https://twitter.com/cgcardona'>gabr13l</a>
           </div>
         </div>
       </div>
