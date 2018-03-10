1677c4d6e05bdffad754eae010baa21547adbda9
Gabriel Cardona <gabriel.cardona@clone.earth>
2018-03-09 17:00:16 -0800

    remove testnet.

--- src/components/BitboxPaper.js
+++ src/components/BitboxPaper.js
@@ -13,7 +13,7 @@ class BitboxPaper extends Component {
         <h2 className="content-head is-center"><i className="fas fa-folder" /> Bitbox Paper</h2>
         <div className="pure-g">
           <div className="pure-u-1-1">
-            <p>Generate a paper-wallet.html w/ private key WIF and public address in cashaddr or legacy encoding for mainnet or testnet. The paper wallet will also list the mnemonic and HD path.</p>
+            <p>Generate a paper-wallet.html w/ private key WIF and public address in cashaddr or legacy encoding. The paper wallet will also list the mnemonic and HD path.</p>
             <p>The privateKey/address pair are created from a fresh 32 byte mnemonic and derived as the first external change address from the following HD path: <code>m / 44&rsquo; / 145&rsquo; / 0&rsquo; / 0 / 0</code>.</p>
             <SyntaxHighlighter language='text' style={ocean}>{`
     Usage: paper [options]
@@ -23,7 +23,6 @@ class BitboxPaper extends Component {
     Options:
 
       -e, --encoding <encoding>  The encoding to use. Options include "cashaddr" and "legacy". Default: "cashaddr"
-      -n, --network <network>    The network to use. Options include "mainnet" and "testnet". Default: "mainnet"
       -h, --help                 output usage information
 
       $ bitbox paper
